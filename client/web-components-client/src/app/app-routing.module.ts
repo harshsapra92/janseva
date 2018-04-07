3afe7ddb3b6b827a5fdae04f16cdf3823829c51c
Harsh Sapra <harsh.sapra92@gmail.com>
2018-04-07 11:54:02 +0530

    Integrated routing with all the components

--- client/web-components-client/src/app/app-routing.module.ts
+++ client/web-components-client/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { UserRegistrationComponent } from "./user-registration/user-registration.component";
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { RequestFormComponent } from './request-form/request-form.component';
+import { RequestExpandedViewComponent } from './request-expanded-view/request-expanded-view.component';
 
 const routes: Routes = [
-  { path:'register',  component: UserRegistrationComponent},
-  { path: 'dashboard', component: DashboardComponent}
+  { path:'register',  component: RegisterComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'newRequest', component: RequestFormComponent },
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'request/:id', component: RequestExpandedViewComponent}
 ];
 
 @NgModule({
